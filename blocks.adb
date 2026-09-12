d25fbf39b99897ad2e89a06f0b825de29b628262
Harris (US), Larry C <larry.c.harris2@boeing.com>
2026-09-11 20:16:07 -0400

    udpates

--- blocks.adb
+++ blocks.adb
@@ -8,118 +8,161 @@ procedure Blocks is
    use Float_Io, Int_Io;
 
 
+
    -- ------TYPES-------------
-   type Coord is 
+   type Type_Float_Array4 is array (0 .. 3) of Float;
+
+   -- For Coordinates --
+   type Type_Coord is 
       record 
          X : Integer range -2 .. 2;
          Y : Integer range -2 .. 2;
       end record;
 
-   type Shape_Coord is array (0 .. 3) of Coord;
+   -- For Rows --
+   type Type_Rows_Coords is array (0 .. 180) of Type_Coord;
 
+   type Type_Rows is
+      record
+         Coords : Type_Rows_Coords;
+         Count :  Integer;
+      end record
 
-   type Float_Array is array (0 .. 3) of Float;
-   
+   -- For Shapes --
+   type Type_Shape_Coord is array (0 .. 3) of Type_Coord;
+
+   type Type_Ltr_Shape_Coord is
+      record
+         Ltr : Character;
+         Coord : Type_Shape_Coord;
+      end record;
+
+   type Type_Shape is 
+      record
+         Relative_Coords : Type_Shape_Coord;
+         Coords : Type_Shape_Coord;
+         Center_Coord : Type_Coord;
+
+      end record;
+
+   -- For Cardinal Directions  & Coordinates --
+   type Type_String_Coord is
+      record
+         Str : String;
+         Coord : Type_Coord;
+      end record;
+
+   -- For Key Translation and Rotation Coordinates --
+   type Type_Char_String is
+      record
+         Char : Character;
+         Str: String;
+      end record;
+
 
    -- ------CONSTANTS--------
    Instructions : constant String := "a:left,d:right,w:drop,x:down,k:cw,l OR s:cw,";
    Rtn : Integer := 0;
-   Shape_Coord_I : constant Shape_Coord := ((0,1), (0,0), (0,-1), (0,-2));
-   Shape_Coord_J : constant Shape_Coord := ((0,1), (0,0), (0,-1), (-1,-1));
-   Shape_Coord_L : constant Shape_Coord := ((0,1), (0,0), (0,-1), ( 1,-1));
-   Shape_Coord_S : constant Shape_Coord := ((-1,0), (0,0), (0,1), (1,1));
-   Shape_Coord_Z : constant Shape_Coord := ((-1,1), (0,0), (0,1), (1,0));
-   Shape_Coord_O : constant Shape_Coord := ((1,0), (0,0), (0, 1), ( 1,1));
-   Shape_Coord_T : constant Shape_Coord := ((-1,0), (0,0), (1,0), (0,1));
 
-   Lttrs : constant String := "IJLSZOT";
+   Ltr_Shape_Coord_Array      : array(0 .. 6) of Type_Ltr_Shape_Coord;
+   Ltr_Shape_Coord_Array(0)   := ('I', (( 0, 1), (0,0), ( 0,-1), ( 0,-2)));
+   Ltr_Shape_Coord_Array(1)   := ('J', (( 0, 1), (0,0), ( 0,-1), (-1,-1)));
+   Ltr_Shape_Coord_Array(2)   := ('L', (( 0, 1), (0,0), ( 0,-1), ( 1,-1)));
+   Ltr_Shape_Coord_Array(3)   := ('S', ((-1, 0), (0,0), ( 0, 1), ( 1, 1)));
+   Ltr_Shape_Coord_Array(4)   := ('Z', ((-1, 1), (0,0), ( 0, 1), ( 1, 0)));
+   Ltr_Shape_Coord_Array(5)   := ('O', (( 1, 0), (0,0), ( 0, 1), ( 1, 1)));
+   Ltr_Shape_Coord_Array(6)   := ('T', ((-1, 0), (0,0), ( 1, 0), ( 0, 1)));
 
-   Up : constant Coord := (0,1);
-   Down : constant Coord := (0,-1);
-   Left : constant Coord := (-1,0);
-   Right : constant Coord := (1, 0);
+   Range_Ltrs : constant array(0 .. 6) of Integer := (0,1,2,3,4,5,6);
 
-   Space : Character := '.';
+   Card : array(0 .. 6) of Type_String_Coord := (("up",( 0, 1)), ("down",( 0,-1)), ("right",( 1, 0)), ("left",( 1, 0)));
+
+   Space : constant Character := '.';
    N_Spaces : constant Integer := 10;
    Range_Spaces : constant array(0 .. 9) of Integer := (0,1,2,3,4,5,6,7,8,9);
    Block : constant Character := 'O';
+
    N_Rows : constant Integer := 20;
+
+   Coord_Reset : constant Type_Coord := ( -1, -1);
    
-   Matrix_Field : constant array(0 .. 19, 0 .. 9) of Character := (others => (others => Space));
-   Range_Rows : constant array(0 .. 19) of Integer:= (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19);
+   Key_Translations  : array(0 .. 7) of Type_Char_String := ( ('a', "left"), ('l', "left"), ('d', "right), (''', "right"), ('w', "down"), ('p', "down"), ('x', "down"), ('/', "down"), ('u', "up"));
+   Key_Rotations     : array(0 .. 2) of Type_Char_String := ( ('s', "cw"), (';', "ccw") );
+
    
+
+
+
+
+
+
+
+
+
+
+
+
+   -- VARIABLES ----
+   
+   Shape : Type_Shape := ((),(),());
+
+   Rows  : Type_Rows;
+   Rows.Coords := (others => Coord_Reset); 
+   Rows.Count  := 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    -- -------FUNCTIONS -----------
-   function Rotation_CW(Input_Coord : Coord) return Coord is
-      New_Coord : Coord;
+   -- Array Access
+
+
+   -- Constants
+   function Get_Lttr_Type_Coord(ltr : Character) return Type_Coord_Array4 is
+      Lttr_Type_Coord : Type_Coord_Array4;
    begin
-      New_Coord.X := Input_Coord.Y;
-      New_Coord.Y := -1 * Input_Coord.Y;
-      return New_Coord;
+      case 
+
+   end Get_Lttr_Type_Coord;
+
+
+
+
+
+
+
+
+
+
+   function Rotation_CW(Input_Type_Coord : Type_Coord) return Type_Coord is
+      New_Type_Coord : Type_Coord;
+   begin
+      New_Type_Coord.X := Input_Type_Coord.Y;
+      New_Type_Coord.Y := -1 * Input_Type_Coord.Y;
+      return New_Type_Coord;
    end Rotation_CW;
 
-   function Rotation_CCW(Input_Coord : Coord) return Coord is
-      New_Coord : Coord;
+   function Rotation_CCW(Input_Type_Coord : Type_Coord) return Type_Coord is
+      New_Type_Coord : Type_Coord;
    begin
-      New_Coord.X := -1 * Input_Coord.Y;
-      New_Coord.Y := Input_Coord.Y;
-      return New_Coord;
+      New_Type_Coord.X := -1 * Input_Type_Coord.Y;
+      New_Type_Coord.Y := Input_Type_Coord.Y;
+      return New_Type_Coord;
    end Rotation_CCW;
 
    -- -------PACKAGES -----------
-   package Shape is
-      Rel_Coords : Coord;
-      Coords : Coord;
-      Ctr : Integer;
-      function Init return Integer;
-      function Update_Coords return Integer;
-      function Rotate return Integer;
-      function Translate return Integer;
-   end Shape;
 
-   package body Shape is
-      Rtn : Integer := 0;
-      function Init return Integer is
-         Rtn : Integer := 0;
-      begin
-         Put_Line("DEBUG.Shape_Init");
-         return Rtn;
-      end Init;
-
-      function Update_Coords return Integer is
-         Rtn : Integer := 0;
-      begin
-         Put_Line("DEBUG.Shape_Update_Coords");
-         return Rtn;
-      end Update_Coords;
-
-      function Rotate return Integer is
-         Rtn : Integer := 0;
-      begin
-         Put_Line("DEBUG.Shape_Rotate");
-         return Rtn;
-      end Rotate;
-
-      function Translate return Integer is
-         Rtn : Integer := 0;
-      begin
-         Put_Line("DEBUG.Shape_Translate");
-         return Rtn;
-      end Translate;
-
-   end Shape;
-
-   -- ------Rows-----------
-   package Rows is
-      Rtn : Integer := 0;
-      function Update_Coords return Integer;
-   end Rows;
-   package body Rows is
-      function Update_Coords return Integer is
-      begin
-         Put_Line("DEBUG.Rows_Update_Coords");
-         return Rtn;
-      end Update_Coords;
-   end Rows;
+   
 
    -- ----------Main Functions---------------
    function Get_New_Shape return Integer is
@@ -174,14 +217,15 @@ procedure Blocks is
    
 
    -- -------------Main-----------------
-   function Main return Integer is
+   procedure Run is
 
-      Rtn : Integer :=  0;
+
       Key : Character  := 'x'; 
       Ctr : Integer :=  0;
       Score : Integer := 0;
       Lttr : Character := 'L';
-      
+      Matrix_Field : array(0 .. 19, 0 .. 9) of Character := (others => (others => Space));
+      Rows : array(<>)
    begin
       Rtn := Get_New_Shape;
 
@@ -221,12 +265,12 @@ procedure Blocks is
 
       return Rtn;
 
-   end Main;
+   end Run;
 
 begin
    -- Gather User Inputs
    Put_Line("INFO. Instructions:" & Instructions );
    New_Line;
-   Rtn := Main;
+   Run;
    
 end Blocks;
--- blocks.py
+++ blocks.py
@@ -8,7 +8,7 @@ import msvcrt
 
 ########### UTILS ##################
 # Constants
-INSTRUCTIONS = "a:left,d:right,w:drop,x:down,k:cw,l OR s:cw,"
+INSTRUCTIONS = "a OR ;=left,s OR '=right,x OR /=down,w OR p=drop,d OR l:cw,f or k:cw,"
 LTTR_COORD = {
     "I":[[ 0,1], [0,0], [0,-1], [ 0,-2]],
     "J":[[ 0,1], [0,0], [0,-1], [-1,-1]],
@@ -23,24 +23,35 @@ CARD = {
     "up": [0,1], "down": [0, -1],"right": [1,0], "left": [-1,0],
 }
 SPACE = "."
+
 N_SPACES = 10
 RANGE_SPACES = range(N_SPACES)
 ROW = [SPACE for x in RANGE_SPACES]
 BLOCK = "O"
+ROWS_BLOCK = "H"
 N_ROWS = 20
-MATRIX = [copy.deepcopy(ROW) for x in range(N_ROWS)]
 RANGE_ROWS = range(N_ROWS)
+MATRIX = [copy.deepcopy(ROW) for x in range(N_ROWS)]
+
 KEYS_TRANSLATIONS = {
     "a": "left",
+    "l": "left",
+
     "d": "right",
-    "e": "up",
+    "'": "right",
+    
     "w": "down",
+    "p": "down",
+
     "x": "down",
+    "/": "down",
+
+    "u": "up"
 }
 KEYS_ROTATIONS = {
-    "l": "ccw",
-    "k": "cw",
     "s": "cw",
+
+    ";": "ccw",
 }
 # Functions
 
@@ -74,176 +85,157 @@ def getch():
             key_char = key.decode('utf-8', errors='ignore')
             #print(f"INFO. You pressed: {key_char} (Raw: {key})")
             break
-
+    
     return key_char.lower()
 
-# Classes
+################## CLASSES ######################
+
 class Shape(object):
-
-    def __init__(self, rel_coords):
-        self.rel_coords = rel_coords
-        self.coords = copy.deepcopy(rel_coords)
-        self.ctr = [5,16]
-        self.update_coords()
-
-    def update_coords(self):
-        for ii, coord in enumerate(self.coords):
-
-            self.coords[ii][0] = self.ctr[0] + self.rel_coords[ii][0]
-            self.coords[ii][1] = self.ctr[1] + self.rel_coords[ii][1]
-
-    def rotate(self, _dir):
-        if _dir == "cw":
-            for ii, rel_coord in enumerate(self.rel_coords):
-                self.rel_coords[ii] = unit_rotation_cw(self.rel_coords[ii])
-        elif _dir == "ccw":
-            for ii, rel_coord in enumerate(self.rel_coords):
-                self.rel_coords[ii] = unit_rotation_ccw(self.rel_coords[ii])
-
-        self.update_coords()
-
-    def translate(self, card):
-        if card in KEYS_TRANSLATIONS.values():
-            self.ctr[0] += CARD[card][0]
-            self.ctr[1] += CARD[card][1]
-
-        self.update_coords()
-
-class MatrixRows(object):
     def __init__(self):
-        self.coords =  [[x,-1] for x in RANGE_ROWS]
-        self.coords += [[x, 0] for x in [0,1,2,3,5,6,7,8,9]]
-        self.coords += [[x, 1] for x in [1,2,6,7,8]]
-        self.updated = False
+        self.relative_coords = []
+        self.coords = []
+        self.center_coord = []
 
-    def update_coords(self, new_coords):
-        self.coords += new_coords
 
 ######################################## MAIN CLASS  ########################################
 class Blocks(object):
     
     def __init__(self, args):
-        self.matrix = copy.deepcopy(MATRIX)
-        self.rows = MatrixRows()
-        self.complete_row = []
         self.score = 0
-        self.next_letter = random.choice(NORM_LTTR_COORD_KEYS)
+        self.rows = copy.deepcopy(MATRIX);
+        self.shape = Shape()
+        self.next_letter = "I" # random.choice(NORM_LTTR_COORD_KEYS)
         
+
+    def update_coords(self):
+        for ii, coord in enumerate(self.shape.coords):
+
+            self.shape.coords[ii][0] = self.shape.center_coord[0] + self.shape.relative_coords[ii][0]
+            self.shape.coords[ii][1] = self.shape.center_coord[1] + self.shape.relative_coords[ii][1]
+
+    def rotate(self, _dir):
+            if _dir == "cw":
+                for ii, rel_coord in enumerate(self.shape.relative_coords):
+                    self.shape.relative_coords[ii] = unit_rotation_cw(self.shape.relative_coords[ii])
+            elif _dir == "ccw":
+                for ii, rel_coord in enumerate(self.shape.relative_coords):
+                    self.shape.relative_coords[ii] = unit_rotation_ccw(self.shape.relative_coords[ii])
+    
+            self.update_coords()
+    
+    def translate(self, card):
+        if card in KEYS_TRANSLATIONS.values():
+            self.shape.center_coord[0] += CARD[card][0]
+            self.shape.center_coord[1] += CARD[card][1]
+
+        self.update_coords()
+
     def move_shape(self, key):
             if key in KEYS_TRANSLATIONS.keys():
                 move = KEYS_TRANSLATIONS[key]
-                self.shape.translate(move)
+                self.translate(move)
             elif key in KEYS_ROTATIONS.keys():
                 move = KEYS_ROTATIONS[key]
-                self.shape.rotate(move)
+                self.rotate(move)
 
-    def check_shape_landed(self):
-            for rc in self.rows.coords:
-                for tc in self.shape.coords:
-                    if tc[0] == rc[0] and tc[1] == rc[1]:
-                        self.move_shape("e")
-                        return True
+    def shape_touched_down(self):
+        shape_touched_down = False
+        for coord in self.shape.coords:
+            x,y = coord
+            if y < 0 or self.rows[y][x] == ROWS_BLOCK:
+                self.move_shape("u")
+                shape_touched_down = True
+                break
 
-    def add_shape_to_rows(self):
-        self.rows.update_coords(self.shape.coords)
+        return shape_touched_down
 
+    def update_rows(self):
+        
+        for coord in self.shape.coords:
+            x,y = coord
+            self.rows[y][x] = ROWS_BLOCK
+        
     def remove_full_rows(self):
         rows_dict = {}
-        self.complete_row = []
-        for coord in self.rows.coords:
-            if coord[1] < 0:
-                continue
-            if coord[1] not in rows_dict.keys():
-                rows_dict[coord[1]] = []
-            rows_dict[coord[1]].append(coord)
+        new_y = 0
+        complete_row = [ROWS_BLOCK for x in RANGE_SPACES]
+        new_rows = copy.deepcopy(self.rows)
 
-        new_coords = [[x,-1] for x in RANGE_ROWS]
-        skeys = sorted(rows_dict.keys())
-        new_y = min(skeys)
-        for y in skeys:
-            if y < 0 or len(rows_dict[y]) < 10:
-                for ii in range(len(rows_dict[y])):
-                    rows_dict[y][ii][1] = new_y
-                new_coords += rows_dict[y]
-                new_y += 1
-            else:
-                self.complete_row += rows_dict[y]
-                
+        for old_y in RANGE_ROWS:
+
+            new_rows[new_y] = copy.deepcopy(self.rows[old_y])
+
+            if self.rows[old_y] == complete_row:
                 self.score += 10
-
-        if len(self.complete_row) > 0:
-            print("INFO: line(s) removed!")
-            self.update_matrix()
-            time.sleep(0.5)
-            self.complete_row = []
+            else:
+                new_y += 1
             
-        self.rows.coords = new_coords
-
-    def get_new_shape(self):
-        self.shape = Shape(copy.deepcopy(LTTR_COORD[self.next_letter]))
-        self.next_letter = random.choice(NORM_LTTR_COORD_KEYS)
-
-    def update_matrix(self):
-
-        self.matrix = copy.deepcopy(MATRIX)
-                
-        for coord in self.shape.coords:
-
-            x = coord[0]
-            y = coord[1]
-            if x in RANGE_SPACES and y in RANGE_ROWS:
-                self.matrix[y][x] = "O"
-
-        for coord in self.rows.coords:
-
-            x = coord[0]
-            y = coord[1]
-
-            if x in RANGE_SPACES and y in RANGE_ROWS:
-                self.matrix[y][x] = "H"
-
-        for coord in self.complete_row:
-
-            x = coord[0]
-            y = coord[1]
-            if x in RANGE_SPACES and y in RANGE_ROWS:
-                self.matrix[y][x] = "+"
+            old_y += 1
             
-        self.print_matrix()
+        
+        self.rows = copy.deepcopy(new_rows)
         
     def print_matrix(self):
-        print("Score:", self.score,"Next:", self.next_letter)
+        # print matrix to screen
+        print("\n\n\n\n\n\n\n\nScore:", self.score,"Next:", self.next_letter)
+
         for y in range(N_ROWS):
-            row_str ="                " +  "".join(self.matrix[N_ROWS - 1 - y])
+            y2 = N_ROWS - 1 - y
+            row = copy.deepcopy(ROW)
+            for x in range(N_SPACES):
+                if [x,y2] in self.shape.coords:
+                    row[x] = BLOCK
+                elif self.rows[y2][x] == ROWS_BLOCK:
+                    row[x] = ROWS_BLOCK
+            row_str ="                " +  "".join(row)
             print(row_str)
 
+    def get_new_shape(self):
+        self.shape.relative_coords = copy.deepcopy(LTTR_COORD[self.next_letter])
+        self.shape.coords = copy.deepcopy(self.shape.relative_coords)
+        self.shape.center_coord = [4,17]
+        self.update_coords()
+        self.next_letter = random.choice(NORM_LTTR_COORD_KEYS)
+
     def run(self):
-        key = "x"
-        ctr = 0
-        self.get_new_shape()
+
+        key = "n"
+        ctr = 1
         time1 = datetime.now()
+        time_diff = 0
+        self.get_new_shape()
+        
         while key != "q":
+
             self.move_shape(key)   
-            if self.check_shape_landed():
-                self.add_shape_to_rows()
+
+            if self.shape_touched_down():
+                # Add shape to rows
+                self.update_rows()
                 self.remove_full_rows()
                 self.get_new_shape()
                 key = "x"
                 self.score += 1
 
-            self.update_matrix()
-            if key != "w":
+            self.print_matrix()
+            
+            if key not in ("w","p"):
                 if ctr % 2 == 0:
                     key = getch()
+                    print("DEBUG. Key", key)
+                    
                 else:
-                    if time2 > 0.5:
+                    if time_diff > 0.5:
                         key = "x"
                     else:
                         key = "n"
+
             ctr += 1
-            time2 = (datetime.now() - time1).total_seconds()
+            time_diff = (datetime.now() - time1).total_seconds()
             time1 = datetime.now()
-    
+
+            
+
 # Main Function
 def main(args):
     print("INFO. Instructions:", INSTRUCTIONS)
